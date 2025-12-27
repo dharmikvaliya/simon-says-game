d4841ffe332c12e717a3e16144c138794abfe62b
Dharmik Valiya <valiyaint@gmail.com>
2025-12-27 22:10:55 +0530

    Intial commit - Simon Says Game

---
+++ app_new.js
@@ -0,0 +1,85 @@
+let gameseq = [];
+let userseq = [];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+let btns = ["yellow" , "red" , "purple" , "green"];
+
+document.addEventListener("keypress" , function () {
+    if(started == false) {
+        console.log("game is started");
+        started = true;
+        levelUp();
+    }
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+
+    setTimeout(function() {
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp() {
+    userseq = [];
+    level++;
+    h2.innerText = `level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randcolor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randcolor}`);
+    gameseq.push(randcolor);
+    console.log(gameseq);
+    gameFlash(randbtn);
+}   
+
+function checkAns(idx) {
+    if(userseq[idx] === gameseq[idx]) {
+        if(userseq.length == gameseq.length) {
+            setTimeout(levelUp , 1000);
+        }
+
+    }else {
+        h2.innerHTML = `GAME OVER !  Your score was <b>${level}</b> <br> press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+}
+
+function btnpress() {
+    let btn = this;
+    userFlash(btn);
+
+   let  usercolor = btn.getAttribute("id");
+    userseq.push(usercolor);
+
+    checkAns(userseq.length-1);
+}
+
+
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns) {
+    btn.addEventListener("click" , btnpress);
+}
+
+function reset() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
+}
